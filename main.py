c8ef894e425b70e5d5a434c50975839b9b610fd2
filipetexa <filipeteixeirasouza@gmail.com>
2024-08-28 11:13:09 -0300

    [create] Create third sorting_algorithm "merge_sort_algorit_algorithm"

--- main.py
+++ main.py
@@ -24,17 +24,12 @@ def generate_randon_array(array_size):
         arr.append(aux_arr)
     return enumerate(arr)
 
-def bubble_sort_algorit_algorithm(list):
-    
-    for i in range(len(list)):
-        for j in range(0, len(list) - i - 1):
-            
-            if list[j] > list[j + 1]:
-                list[j + 1], list[j] = list[j], list[j + 1]
 
+# Algoritimo 3 - Merge Sort Algorithm 
+def merge_sort_algorit_algorithm(list):
+    
     return list
             
-
 # Cria o executor para trabalharmos com paralelismo
 # Chama as threds executoras para executor os algorimos de ordenação
 
@@ -42,13 +37,8 @@ def bubble_sort_algorit_algorithm(list):
 test_list = [10,13,2,9,7]
 print(test_list)
 
-print(bubble_sort_algorit_algorithm(test_list))
 
 
-# Algoritimo 2 - Bubble Sort 
-
-
-# Algoritimo 3
 # Chama thread de visualição de dados.
 
 
--- sorting_algorithms/sorting_algorithms.py
+++ sorting_algorithms/sorting_algorithms.py
@@ -1,29 +1,101 @@
 
 
 # Algoritimo 1 - Selection Sort Algorithm - O(n log n)
-def selection_sort_algorithm(list):
+def selection_sort_algorithm(arr):
     min_value_position = 0
-    list_size = len(list) - 1
-    for i in enumerate(list):
+    arr_size = len(arr) - 1
+    for i in enumerate(arr):
         aux_i = i + 1
         min_value_position = i
-        while aux_i <= list_size:
+        while aux_i <= arr_size:
             # Verifica se o valor de atual é o valor minimo 
-            if list[min_value_position] > list[aux_i]:
+            if arr[min_value_position] > arr[aux_i]:
                 # Segura a posição do menor  valor
                 min_value_position = aux_i
             # aux_i ++
             aux_i += 1
-        list[i], list[min_value_position] = list[min_value_position], list[i]
-    return list
+        arr[i], arr[min_value_position] = arr[min_value_position], arr[i]
+    return arr
 
 # Algoritimo 2 - Bubble Sort Algorithm - O(n^2)
-def bubble_sort_algorit_algorithm(list):
+def bubble_sort_algorit_algorithm(arr):
     
-    for i in range(len(list)):
-        for j in range(0, len(list) - i - 1):
+    for i in range(len(arr)):
+        for j in range(0, len(arr) - i - 1):
             
-            if list[j] > list[j + 1]:
-                list[j + 1], list[j] = list[j], list[j + 1]
+            if arr[j] > arr[j + 1]:
+                arr[j + 1], arr[j] = arr[j], arr[j + 1]
 
-    return list
+    return arr
+
+
+def merge(arr, left, mid, right):
+    n1 = mid - left + 1
+    n2 = right - mid
+
+    # Create temp arrays
+    L = [0] * n1
+    R = [0] * n2
+
+    # Copy data to temp arrays L[] and R[]
+    for i in range(n1):
+        L[i] = arr[left + i]
+    for j in range(n2):
+        R[j] = arr[mid + 1 + j]
+
+    i = 0  # Initial index of first subarray
+    j = 0  # Initial index of second subarray
+    k = left  # Initial index of merged subarray
+
+    # Merge the temp arrays back
+    # into arr[left..right]
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    # Copy the remaining elements of L[],
+    # if there are any
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    # Copy the remaining elements of R[], 
+    # if there are any
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+def merge_sort_algorithm(arr, left, right):
+    if left < right:
+        mid = (left + right) // 2
+
+        merge_sort_algorithm(arr, left, mid)
+        merge_sort_algorithm(arr, mid + 1, right)
+        merge(arr, left, mid, right)
+
+
+"""
+=============================================
+"""
+def print_list(arr):
+    for i in arr:
+        print(i, end=" ")
+    print()
+
+# Driver code
+if __name__ == "__main__":
+    arr = [12, 11, 8, 13, 5, 6, 7]
+    print("Given array is")
+    print_list(arr)
+
+    merge_sort_algorithm(arr, 0, len(arr) - 1)
+
+    print("\nSorted array is")
+    print_list(arr)
