8b3dd9b99777d43cf5e42e5345a19425c7ac9015
filipetexa <filipeteixeirasouza@gmail.com>
2024-09-15 15:02:20 -0300

    [update] add the dash app to show the first graphs

--- main.py
+++ main.py
@@ -11,8 +11,14 @@ apply_decorators_to_function, \
 write_results_on_csv_file, \
 get_execution_time
 
+
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import plotly.express as px
+
 # Definir parametros iniciais
-ARRAY_SIZE = 1000
+ARRAY_SIZE = 100
 SAMPLE_RANGE = range(1000)
 
 # Define um limit de recursao alto para que o merge sort não apresente stack overflow
@@ -75,7 +81,41 @@ for arr in randon_arr:
     decorated_bubble_sort_algorithm(arr)
     decorated_merge_sort_algorithm(arr, left=0, right= len(arr) - 1)
 
-    
-    
-# Call visualization function 
 
+# Call visualization function 
+# Read the output csv's and treat data
+df_selection = pd.read_csv('output/selection_sort_output.csv')
+df_selection['array_size'] = df_selection['result'].apply(lambda x: int(x[1:-1].split(',')[0]))
+df_selection = df_selection[['execution_time', 'array_size']]
+df_selection['algorithm'] = 'Selection Sort'
+
+df_bubble = pd.read_csv('output/bubble_sort_output.csv')
+df_bubble['array_size'] = df_bubble['result'].apply(lambda x: int(x[1:-1].split(',')[0]))
+df_bubble = df_bubble[['execution_time', 'array_size']]
+df_bubble['algorithm'] = 'Bubble Sort'
+
+df_merge = pd.read_csv('output/merge_sort_output.csv')
+df_merge['array_size'] = df_merge['result'].apply(lambda x: int(x[1:-1].split(',')[0]))
+df_merge = df_merge[['execution_time', 'array_size']]
+df_merge['algorithm'] = 'Merge Sort'
+
+dfs = pd.concat([df_merge, df_selection, df_bubble])
+
+# Initialize app
+app = Dash()
+
+# App layout
+app.layout = [
+    html.Div(children='Sort Algorithms Visualization'),
+    # grafico de linhas!!!!
+    html.Div(children='SELECTION SORT'),
+    dcc.Graph(figure=px.line(df_selection, x='array_size', y='execution_time')),
+    html.Div(children='BUBBLE SORT'),
+    dcc.Graph(figure=px.line(df_bubble, x='array_size', y='execution_time')),
+    html.Div(children='MERGE SORT'),
+    dcc.Graph(figure=px.line(df_merge, x='array_size', y='execution_time')),
+    html.Div(children='COMPARAÇÃO'),
+    dcc.Graph(figure=px.line(dfs, x='array_size', y='execution_time', color='algorithm')),
+]
+
+app.run(debug=False)
